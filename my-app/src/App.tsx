4d402f963397ecddf719d5b16254d22de3532487
david <davidrxyang@hotmail.com>
2024-06-02 20:58:52 -0400

    removed unnecessary import

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MyFirstComponent from './components/MyFirstComponent';
 import ArrayPage from './pages/ArrayPage';
